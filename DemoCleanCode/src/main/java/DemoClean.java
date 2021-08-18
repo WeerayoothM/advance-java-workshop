4b5c7a822201f19a80f5341c257cc689c8695e8e
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-18 17:22:21 +0700

    Demo with fail fast and return early

--- DemoCleanCode/src/main/java/DemoClean.java
+++ DemoCleanCode/src/main/java/DemoClean.java
@@ -1,5 +1,41 @@
 public class DemoClean {
     public static void main(String[] args) {
 
+        // Type 1
+        if (true){
+            if (true){
+                if (true){
+                    // TODO Sth
+                }
+            }
+        }
+
+        // Type 2 fail fast , return early
+        if (false) {return ;}
+        if (false) {throw new RuntimeException("2");}
+        if (false) {throw new RuntimeException("3");}
+        // TODO Sth
+    }
+
+    String demo02() {
+        String result = "";
+        if (true){
+            result += "1";
+        }
+        if (true) {
+            result += "2";
+        }
+        return result;
+    }
+
+    String demo03(int score) {
+        String result = "";
+        if (score >= 80){
+            return "A";
+        }
+        if (score >= 70) {
+            return  "B";
+        }
+        return result;
     }
 }
