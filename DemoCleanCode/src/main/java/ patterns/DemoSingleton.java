9074365413b8bce937bb2473de2c30b726401c32
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-20 01:16:56 +0700

    Demo with Singleton pattern

---
+++ DemoCleanCode/src/main/java/ patterns/DemoSingleton.java
@@ -0,0 +1,34 @@
+package patterns;
+
+public class DemoSingleton {
+    public static void main(String[] args) {
+
+        // process1 and process2 is the same Process class ref
+        Process process1 = Process.newInstance();
+        Process process2 = Process.newInstance();
+        System.out.println(process1);
+        System.out.println(process2);
+        process1.start();
+        process2.start();
+    }
+
+}
+
+class Process {
+    private int count = 0;
+    private static Process instance;
+    private Process() {
+    }
+
+    public static Process newInstance() {
+        if (instance == null) {
+            instance = new Process();
+        }
+        return instance;
+    }
+
+    public void start() {
+        count++;
+        System.out.println("Call start method count =" + count);
+    }
+}
