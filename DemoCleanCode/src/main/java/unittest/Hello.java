7904d679f70c930a65aeaa8eb86ee6fededde7ea
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-19 09:44:12 +0700

    Demo with I = Isolate/Independent

--- DemoCleanCode/src/main/java/unittest/Hello.java
+++ DemoCleanCode/src/main/java/unittest/Hello.java
@@ -5,4 +5,17 @@ public class Hello {
     public String hi(String name) {
         return "Hello, " + name;
     }
+
+    public String workWithDb(int id) {
+        // Tight coupling with UserDb class
+        UserDB userDB = new UserDB();
+        return userDB.getNameById(id);
+    }
+}
+
+class UserDB {
+    public String getNameById(int id ){
+        // Unstable with dependency
+        throw new RuntimeException("DB fail");
+    }
 }
