7f5189583e2cdce722d8a2006dbb7bb9052c4c85
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-20 12:38:44 +0700

    Demo with stub and spy

--- DemoCleanCode/src/main/java/testdouble/GenerateIdService.java
+++ DemoCleanCode/src/main/java/testdouble/GenerateIdService.java
@@ -10,7 +10,7 @@ public class GenerateIdService {
         this.random = random;
     }
 
-    public String get(){
+    public String get(Object name) {
         int number = random.nextInt(10);
         String id = "XYZ" + number;
         return id;
