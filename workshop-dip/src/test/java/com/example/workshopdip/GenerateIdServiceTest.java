adde63d3fafc12621c9a95026ca33e6b4e41dd78
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-20 18:02:04 +0700

    test with SpringBootTest

--- workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceTest.java
+++ workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 
 @SpringBootTest
 class GenerateIdServiceTest {
@@ -17,14 +18,13 @@ class GenerateIdServiceTest {
     @Autowired
     private GenerateIdService generateIdService;
 
-    @MockBean
+    @MockBean // @SpringBootTest ทำให้ Random ใช้ตัวนี้แทน และไม่ต้องทำ injection เอง
     private Random random;
 
     @Test
     @DisplayName("Start springboot test")
     public void case01(){
-        Mockito.when(random.nextInt(10)).thenReturn(7);
-
+        Mockito.when(random.nextInt(anyInt())).thenReturn(7); // stub
         String id = generateIdService.get();
         assertEquals("XYZ7",id);
     }
