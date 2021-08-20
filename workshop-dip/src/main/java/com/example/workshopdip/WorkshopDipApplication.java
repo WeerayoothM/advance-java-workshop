f2a72ffc15a1d3374c2aefca4f5424740c89d637
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-08-20 17:00:55 +0700

    Create Random Bean in String context

--- workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
+++ workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
@@ -2,9 +2,17 @@ package com.example.workshopdip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class WorkshopDipApplication {
+
+	@Bean
+	public Random createMyRandom(){
+		return new Random();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopDipApplication.class, args);
--- workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceTest.java
+++ workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceTest.java
@@ -2,8 +2,10 @@ package com.example.workshopdip;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Random;
 
@@ -15,9 +17,14 @@ class GenerateIdServiceTest {
     @Autowired
     private GenerateIdService generateIdService;
 
+    @MockBean
+    private Random random;
+
     @Test
     @DisplayName("Start springboot test")
     public void case01(){
+        Mockito.when(random.nextInt(10)).thenReturn(7);
+
         String id = generateIdService.get();
         assertEquals("XYZ7",id);
     }
